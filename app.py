6ca3e9cc7b95b0ba1d4c44deaeb83a2927fe368c
Zayland <you@example.com>
2025-03-02 12:57:11 +0300

    mobile_chaeck

--- app.py
+++ app.py
@@ -1,43 +1,18 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import urllib.parse
+from flask import Flask, render_template
 
-class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html; charset=utf-8')
-            self.end_headers()
-            with open('index.html', 'rb') as file:
-                self.wfile.write(file.read())
-        elif self.path == '/style.css':
-            try:
-                with open('style.css', 'rb') as file:
-                    self.send_response(200)
-                    self.send_header('Content-type', 'text/css')
-                    self.end_headers()
-                    self.wfile.write(file.read())
-            except FileNotFoundError:
-                self.send_error(404, "File not found")
-        elif self.path == '/favicon.ico':
-            # Игнорируем запросы к favicon.ico
-            self.send_response(204)
-            self.end_headers()
-        else:
-            self.send_error(404, "Page not found")
+app = Flask(__name__)
 
-    def send_error(self, code, message=None):
-        try:
-            if message:
-                message = message.encode('utf-8').decode('latin-1')
-            super().send_error(code, message)
-        except UnicodeEncodeError:
-            super().send_error(code, "Page not found")
+@app.route("/")
+def invitation():
+    return render_template("index.html")
 
-def run(server_class=HTTPServer, handler_class=RequestHandler, port=8000):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f'Сервер запущен на http://localhost:{port}')
-    httpd.serve_forever()
+@app.route("/venue")
+def venue():
+    return render_template("venue.html")
 
-if __name__ == '__main__':
-    run()
+@app.route("/registration")
+def registration():
+    return render_template("registration.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
